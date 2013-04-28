ade5f1e582d32d9b7ece8915da158fdb5e399b43
Anthony Kazyaka <ask.anthony@hotmail.com>
2013-04-28 06:49:38 -0400

    Adding a lot of JavaScript functionality to the site.
    Makes the site look usable.

--- Final/js/app.js
+++ Final/js/app.js
@@ -1,1 +1,24 @@
-﻿
+﻿$(document).ready(function () {
+
+    $(document).on('click', '.askButton', function () {
+        var questionInputs = $(".question");
+
+        alert('Your question asking:\n\n"' + questionInputs[0].value + '",\nis of the type "' + questionInputs[1].value + '",\ncovers these topics: "'
+            + questionInputs[2].value + '",\nand is aimed at "' + questionInputs[3].value + '"\n\n has been submitted!');
+
+        for (var i = 0; i < questionInputs.length; i++) {
+            questionInputs[i].value = "";
+        }
+    });
+
+    $(document).on('click', '#nickelback', function () {
+        var responses = $("#nickelbackFreeResponses > li");
+        console.log(responses);
+        var index = responses[responses.length - 1].textContent.substr(0, 2);
+
+        $("#nickelbackResponses > ul").html(responses);
+
+    });
+
+
+});
